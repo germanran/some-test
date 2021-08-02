a114f5b6d84c527e90b9597bd16ff9120cd3d37d
Ran German <rangerman44@gmail.com>
2021-08-02 22:01:17 +0300

    my first commit of this c# test working with git and github

---
+++ Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace udemyCourse
+{
+    public class Person
+    {
+        public string name;
+        public int age;
+        public string work;
+        public int[] numbers;
+
+        public Person(string name, int age, string work)
+        {
+            this.name = name;
+            this.age = age;
+            this.work = work;
+            numbers = new int[6];
+            for (int i = 0; i < 6; i++)
+            {
+                this.numbers[i] = i + 2;
+            }
+        }
+
+        public void sayHi()
+        {
+            Console.WriteLine("hello my friend {0} of the age of {1} and who works as {2}," +
+                "and the numbers are: {3} ",
+                this.name, this.age, this.work, string.Join(',', this.numbers));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using udemyCourse;
+
+namespace udemyCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string>();
+            string input;
+            string resultMessage = "";
+            while (true)
+            {
+                Console.WriteLine("Please enter names");
+                input = Console.ReadLine();
+
+                if (input.Length > 0)
+                {
+                    names.Add(input);
+                }
+                else
+                {
+                    break;
+                }
+
+      
+            }
+            
+
+            switch (names.Count)
+            {
+                case 0:
+                    break;
+                case 1:
+                    resultMessage = string.Format("{0} likes your post", names[0]);
+                    break;
+                case 2:
+                    resultMessage = string.Format("{0} and {1} likes your post", names[0], names[1]);
+                    break;
+                default:
+                    resultMessage = string.Format("{0}, {1} and others like your post", names[0], names[1]);
+                    break;
+            }
+            Console.WriteLine(resultMessage);
+        }
+    }
+}
+
